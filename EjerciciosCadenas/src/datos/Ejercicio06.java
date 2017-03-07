830366bc63b87a0dd6119cfd90014079f2baa7d3
Álvaro Sánchez <alvarosanchez1998@gmail.com>
2017-03-07 14:28:16 +0100

    Update Ejercicio06.java
    Sin punto

--- EjerciciosCadenas/src/datos/Ejercicio06.java
+++ EjerciciosCadenas/src/datos/Ejercicio06.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 
 public class Ejercicio06 {
 //	Crear una clase que tome como cadena a cifrar el argumento que se haya pasado por la
-//	línea de comandos, la pase a mayúsculas, la cifre usando una clave aleatoria entre 0 y
-//	10. Por último, muestre en pantalla la cadena original, la clave de cifrado y la cadena
-//	cifrada. 
+//	lÃ­nea de comandos, la pase a mayÃºsculas, la cifre usando una clave aleatoria entre 0 y
+//	10. Por Ãºltimo, muestre en pantalla la cadena original, la clave de cifrado y la cadena
+//	cifrada 
 	
 	private static Scanner sc  = new Scanner (System.in);
 	
