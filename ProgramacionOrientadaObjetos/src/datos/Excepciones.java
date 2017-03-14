e0314e965add19eee726584c62b70d44fda5fef0
Alvaro <alvarosanchez1398@gmail.com>
2017-03-14 14:07:28 +0100

    Explicacion Excepciones

--- ProgramacionOrientadaObjetos/src/datos/Excepciones.java
+++ ProgramacionOrientadaObjetos/src/datos/Excepciones.java
@@ -3,8 +3,16 @@ package datos;
 public class Excepciones {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			int d = 0;
+			int a = 42/d;
+			System.out.println(a);
+		} catch (ArithmeticException | NullPointerException e) { //--> Si al producirse dos errores debe de hacer lo mismo se pone esto
+			System.out.println("Division / zero");
+			System.out.println("Excepcion: "+e);
+		} catch (Exception e) {
+			System.out.println();
+		}//--> Esta es la excepcion generica siempre al final
+		System.out.println("Fin");
 	}
-
 }
---
+++ ProgramacionOrientadaObjetos/src/datos/Excepciones2.java
@@ -0,0 +1,24 @@
+package datos;
+
+public class Excepciones2 {
+
+	public static void main(String[] args) {
+//		try {
+			meotodo();
+//		} catch (Exception e) {
+//			System.out.println("Eception "+e);
+//		}
+
+	}
+
+	private static void meotodo() throws NullPointerException {
+//		try {
+			throw new NullPointerException("mensaje");//--> Lanza la expcion
+//		} catch (NullPointerException e) {
+//			System.out.println("Exepcion capturada");
+//			throw e;//-->Lanza otra vez la excepcion
+//		}
+		
+	}
+
+}
